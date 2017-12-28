51bfe4df69ec4ff0dfde9a74818403b4a8b0508f
Kary <karina@arteko.mx>
2017-12-28 10:33:53 -0600

    add project page

--- server.js
+++ server.js
@@ -60,6 +60,12 @@ app.get('/about', (req, res) => {
 	})
 })
 
+app.get('/projects', (req, res) => {
+	res.render('projects.hbs', {
+		pageTitle: 'Projects head',
+	})
+})
+
 app.get('/bad', (req, res) => {
 	res.send({
 		errorMessage: 'errorMsg'
